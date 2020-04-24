529a20f02f180d4db3fa8f48ffdc6d90296176c5
Evgenii Harichev <harichev@cs.petrsu.ru>
2020-04-24 14:40:55 +0300

    commit 23

--- app/y_line.c
+++ app/y_line.c
@@ -20,7 +20,7 @@ int y_line(text txt, const char* contents) {
     // Проверка на переполнение
     if(txt->cursor->position + contents_len <= MAXLINE) {
         // Копируем в позицию курсора и далее
-       strncpy(line->contents + txt->cursor->position-1, contents,strlen(contents));
+       strncpy(line->contents + txt->cursor->position-1, contents, strlen(contents));
         // Все что после новой подстроки обрезаем
         line->contents[txt->cursor->position + contents_len-1] = '\n';
 	line->contents[txt->cursor->position + contents_len] = '\0';
